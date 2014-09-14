37582249c41650ac68171a17553004e5c915dd4a
elnazd <elnaz.delpisheh@gmail.com>
2014-09-14 12:04:54 -0400

    Create starry.c

---
+++ starry.c
@@ -0,0 +1,9 @@
+/*This is my second C program!*/
+#include<stdio.h>
+int main(void){ //This is a special function!
+
+	
+	printf("It is a starry night!\n");
+	printf("*\n**\n***\n****\n*****\n");
+	return 0;
+}
