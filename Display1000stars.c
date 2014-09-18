1cee83c2dad01d9e5fa532d7673c939c2884ad08
elnazd <elnaz.delpisheh@gmail.com>
2014-09-17 21:38:58 -0400

    Create Display1000stars.c

---
+++ Display1000stars.c
@@ -0,0 +1,13 @@
+/*Write a program to print  1000 stars onto the display.*/
+#include<stdio.h>
+int main()
+{
+	int counter=0;
+	while(counter < 1000)
+	{
+		printf("*");
+		counter = counter + 1; //counter++;
+	
+	}
+	return 0;
+}
