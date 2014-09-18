27c16f49232f773f9d6d465e7ad0e931a9a8b5d9
elnazd <elnaz.delpisheh@gmail.com>
2014-09-17 21:39:42 -0400

    Create display100Integers.c

---
+++ display100Integers.c
@@ -0,0 +1,16 @@
+/*
+Write a program to print all positive numbers on the screen:
+1,2,3,4,5,….,100
+*/
+#include<stdio.h>
+int main()
+{
+	int counter=1;
+	while(counter<=100)
+	{
+		printf("%d\n",counter);
+		counter++;
+	
+	}
+	return 0;
+}
